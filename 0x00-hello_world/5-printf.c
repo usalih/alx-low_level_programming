3acae9b32c64c7b42b45423e76105b0880c1d0c7
usman saliu <usalih@yahoo.com>
2023-03-17 02:52:22 +0000

    printf command file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
+ * main - A program that prints a line using the printf function
  * Return: Always 0 (Succes)
  */
-int main (void)
+int main(void)
 {
 printf("with proper english, but the outcome is a piece of art,\n");
 return(0);
