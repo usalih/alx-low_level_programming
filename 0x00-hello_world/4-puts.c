63e4a28b02afc34b7e232b28d0cf3c85d32a0ad6
usman saliu <usalih@yahoo.com>
2023-03-17 02:31:57 +0000

    put command file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
-	    puts("Programming is like building a multilingual puzzle,");
-	        return 0;
+/**
+ * main - A program that prints a line with puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
