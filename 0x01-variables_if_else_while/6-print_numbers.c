2ea7f4017bf882db2b4d494400ed03aa686dc4f7
usman saliu <usalih@yahoo.com>
2023-03-20 23:29:54 +0000

    this code print number from zero to 8

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the single number from 1 to zero
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 8; i++)
+	{
+		printf("%d", i)
+	}
+	putchar('\n');
+	return (0);
+}
