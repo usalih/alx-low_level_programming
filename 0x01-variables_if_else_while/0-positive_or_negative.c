8937767db13c9f5093dccf31a243d33ebfb5b5c5
usman saliu <usalih@yahoo.com>
2023-03-20 01:08:17 +0000

    print a random number and decide if it is positive or not

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,13 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
  if (n > 0) {
-    printf("%d is positive\n");
- } else if (n == 0) {
-    printf("%d is zero\n")	;
- } else {
-    printf("%d is negative\n");
+    printf("%d is positive\n", n);
+ } 
+ else if (n == 0) {
+    printf("%d is zero\n", n);
+ } 
+ else {
+    printf("%d is negative\n", n);
  }
 return (0);
 }
