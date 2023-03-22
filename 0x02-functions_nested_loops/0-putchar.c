183ec48aaa84efd4d7cff1479506d5057668438e
usman saliu <usalih@yahoo.com>
2023-03-22 09:42:56 +0000

    a function to print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "_putchar.c"
+#include "main.h"
+
+/**
+ * main - the program would print '_putchar'
+ *
+ * Return - 0(Success)
+ *
+ */
+int _putchar(char)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return 0;
+}
