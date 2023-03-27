9598479394edd20b1f99cbe4d7fd1c12b843e168
usman saliu <usalih@yahoo.com>
2023-03-27 22:35:48 +0000

    pointer with int parameter and update it value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * reset_to_98 - this function takes the value of an int as a parameter
+ * @n:- the parameter ia a pointer (*n) and value is updated to 98
  * return: Always 0
  */
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
